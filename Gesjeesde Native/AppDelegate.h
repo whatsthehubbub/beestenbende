e90c9d6f8c796ffd2f2891ca3e82241153a9d981
Alper Çugun <git@alper.nl>
2012-03-21 20:20:43 +0100

    Initial Commit

---
+++ Gesjeesde Native/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Gesjeesde Native
+//
+//  Created by Alper Çuğun on 21/3/12.
+//  Copyright (c) 2012 Aardverschuiving Media. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Gesjeesde Native/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Gesjeesde Native
+//
+//  Created by Alper Çuğun on 21/3/12.
+//  Copyright (c) 2012 Aardverschuiving Media. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
