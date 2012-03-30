f1e67c9caea53e9c45c9212caf035d8968ebe62c
Alper Çugun <git@alper.nl>
2012-03-30 19:53:08 +0200

    Implemented first issue view text

---
+++ Gesjeesde Native/FirstIssueIntroductionViewController.h
@@ -0,0 +1,23 @@
+//
+//  FirstIssueIntroductionViewController.h
+//  Gesjeesde Beesten
+//
+//  Created by Alper Çuğun on 30/3/12.
+//  Copyright (c) 2012 Hubbub. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstIssueIntroductionViewController : UIViewController <UIScrollViewDelegate> {
+    UIScrollView *scrollView;
+    UIPageControl *pageControl;
+}
+
+@property (nonatomic, retain) IBOutlet UIScrollView *scrollView;
+@property (nonatomic, retain) IBOutlet UIPageControl *pageControl;
+
+- (IBAction)scrollPage;
+- (IBAction)previous;
+- (IBAction)next;
+
+@end
---
+++ Gesjeesde Native/FirstIssueIntroductionViewController.m
@@ -0,0 +1,85 @@
+//
+//  FirstIssueIntroductionViewController.m
+//  Gesjeesde Beesten
+//
+//  Created by Alper Çuğun on 30/3/12.
+//  Copyright (c) 2012 Hubbub. All rights reserved.
+//
+
+#import "FirstIssueIntroductionViewController.h"
+
+@interface FirstIssueIntroductionViewController ()
+
+@end
+
+@implementation FirstIssueIntroductionViewController
+
+@synthesize scrollView;
+@synthesize pageControl;
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+    
+    self.scrollView.contentSize = CGSizeMake(self.scrollView.frame.size.width * 2, self.scrollView.frame.size.height);
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+- (IBAction)scrollPage {
+    CGRect frame;
+    frame.origin.x = self.scrollView.frame.size.width * self.pageControl.currentPage;
+    frame.origin.y = 0;
+    frame.size = self.scrollView.frame.size;
+    
+    [self.scrollView scrollRectToVisible:frame animated:YES];
+}
+
+- (IBAction)previous {
+    if (self.pageControl.currentPage > 0) {
+        self.pageControl.currentPage -= 1;
+        
+        [self scrollPage];
+    }
+}
+
+- (IBAction)next {
+    if (self.pageControl.currentPage < 1) {
+        self.pageControl.currentPage += 1;
+        [self scrollPage];
+    } else {
+        [self performSegueWithIdentifier:@"Next" sender:self];
+    }
+}
+
+#pragma mark - UIScrollViewDelegate
+
+- (void)scrollViewDidScroll:(UIScrollView *)scrollView {
+    // From: http://www.iosdevnotes.com/2011/03/uiscrollview-paging/
+    CGFloat pageWidth = self.scrollView.frame.size.width;
+    int page = floor((self.scrollView.contentOffset.x - pageWidth / 2) / pageWidth) + 1;
+    
+    self.pageControl.currentPage = page;
+}
+
+
+@end
