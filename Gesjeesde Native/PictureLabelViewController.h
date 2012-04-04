b8880febdb22336938f0776f1336ffe4d0f4f518
Alper Çugun <git@alper.nl>
2012-04-04 19:31:14 +0200

    Made labeling pictures work for one team

--- Gesjeesde Native/PictureLabelViewController.h
+++ Gesjeesde Native/PictureLabelViewController.h
@@ -9,11 +9,38 @@
 #import <UIKit/UIKit.h>
 
 #import "FeaturePickerViewController.h"
+#import "FeaturePicture.h"
 
 @interface PictureLabelViewController : UIViewController <FeaturePickerViewControllerDelegate> {
     UIButton *featureButton;
+    UIButton *deleteButton;
+    
+    UILabel *teamLabel;
+    UILabel *currentLabel;
+    
+    UIImageView *currentImage;
+    
+    Game *game;
+    Team *currentTeam;
+    
+    int currentPictureIndex;
 }
 
 @property (nonatomic, retain) IBOutlet UIButton *featureButton;
+@property (nonatomic, retain) IBOutlet UIButton *deleteButton;
+@property (nonatomic, retain) IBOutlet UIImageView *currentImage;
+
+@property (nonatomic, retain) IBOutlet UILabel *teamLabel;
+@property (nonatomic, retain) IBOutlet UILabel *currentLabel;
+
+@property (assign) int currentPictureIndex;
+
+@property (strong) Game *game;
+@property (strong) Team *currentTeam;
+
+-(IBAction)next:(id)sender;
+-(IBAction)previous:(id)sender;
+-(IBAction)done:(id)sender;
+-(IBAction)deleted:(id)sender;
 
 @end
--- Gesjeesde Native/PictureLabelViewController.m
+++ Gesjeesde Native/PictureLabelViewController.m
@@ -15,6 +15,16 @@
 @implementation PictureLabelViewController
 
 @synthesize featureButton;
+@synthesize deleteButton;
+@synthesize currentImage;
+
+@synthesize teamLabel;
+@synthesize currentLabel;
+
+@synthesize currentPictureIndex;
+
+@synthesize currentTeam;
+@synthesize game;       
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
@@ -29,6 +39,14 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view.
+    
+    AppDelegate *appDelegate = (AppDelegate *)[[UIApplication sharedApplication] delegate];
+    game = appDelegate.game;
+    
+    self.currentTeam = game.team1;
+    [self.teamLabel setText:[NSString stringWithFormat:@"Team %@", self.currentTeam.getTeamName]];
+    
+    [self displayPicture];
 }
 
 - (void)viewDidUnload
@@ -49,11 +67,53 @@
     }
 }
 
+-(IBAction)next:(id)sender {
+    self.currentPictureIndex = (self.currentPictureIndex+1) % self.currentTeam.featurePictures.count;
+    
+    [self displayPicture];
+}
+
+-(IBAction)previous:(id)sender {
+    self.currentPictureIndex -= 1;
+    if (self.currentPictureIndex < 0) {
+        self.currentPictureIndex += self.currentTeam.featurePictures.count;
+    }
+
+    [self displayPicture];
+}
+
+-(void)displayPicture {
+    FeaturePicture *fp = [self.currentTeam.featurePictures objectAtIndex:currentPictureIndex];
+    
+    self.currentImage.image = fp.image;
+    [self.featureButton setTitle:fp.feature forState:UIControlStateNormal];
+    
+    [self.currentLabel setText:[NSString stringWithFormat:@"Foto %d van %d", self.currentPictureIndex+1, self.currentTeam.featurePictures.count]];
+}
+
+-(IBAction)deleted:(id)sender {
+    FeaturePicture *fp = [self.currentTeam.featurePictures objectAtIndex:currentPictureIndex];
+    fp.deleted = !fp.deleted;
+    
+    if (fp.deleted) {
+        [self.deleteButton setImage:[UIImage imageNamed:@"toggle-trash-on.png"] forState:UIControlStateNormal];
+    } else {
+        [self.deleteButton setImage:[UIImage imageNamed:@"toggle-trash-off.png"] forState:UIControlStateNormal];
+    }
+}
+
+-(IBAction)done:(id)sender {
+    
+}
+
 #pragma mark - FeaturePickerViewControllerDelegate
 
 -(void)featurePickerViewController:(FeaturePickerViewController *)controller didSelectFeature:(NSString *)feature {
 
     [self.featureButton setTitle:feature forState:UIControlStateNormal];
+    
+    FeaturePicture *fp = [self.currentTeam.featurePictures objectAtIndex:currentPictureIndex];
+    fp.feature = feature;
     
     [self.navigationController popViewControllerAnimated:YES];
 }
