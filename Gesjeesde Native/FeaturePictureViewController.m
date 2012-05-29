a1b3f27ef0c758fe16223e8b36f984a8ca9d3191
Alper Çugun <git@alper.nl>
2012-05-29 18:49:57 +0200

    fixed fonts in a better way

--- Gesjeesde Native/FeaturePictureViewController.m
+++ Gesjeesde Native/FeaturePictureViewController.m
@@ -46,9 +46,6 @@
     AppDelegate *appDelegate = (AppDelegate *)[[UIApplication sharedApplication] delegate];
     game = appDelegate.game;
     
-    imagesLabel.font = [UIFont fontWithName:@"Vollkorn-Regular" size:imagesLabel.font.pointSize];
-    timeLabel.font = [UIFont fontWithName:@"Vollkorn-Regular" size:timeLabel.font.pointSize];
-    
     // Find the current team
     team = game.team1;
     if (team.tookFeaturePictures) {
