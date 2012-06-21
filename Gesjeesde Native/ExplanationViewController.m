2c9478c92506d4a9c253616c9d2ccbcecd70167c
Alper Çugun <git@alper.nl>
2012-06-22 02:05:15 +0300

    created file for view controller

---
+++ Gesjeesde Native/ExplanationViewController.m
@@ -0,0 +1,55 @@
+//
+//  ExplanationViewController.m
+//  Beestenbende
+//
+//  Created by Alper Çuğun on 22/6/12.
+//  Copyright (c) 2012 Hubbub. All rights reserved.
+//
+
+#import "ExplanationViewController.h"
+
+@interface ExplanationViewController ()
+
+@end
+
+@implementation ExplanationViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+- (IBAction)next:(id)sender {
+    [[SimpleAudioEngine sharedEngine] playEffect:@"i02_schermverder.wav"];
+    
+    [self performSegueWithIdentifier:@"Next" sender:self];
+}
+
+- (IBAction)back:(id)sender {
+    [[SimpleAudioEngine sharedEngine] playEffect:@"i03_schermterug.wav"];
+    
+    [self.navigationController popViewControllerAnimated:YES];
+}
+
+@end
