f6157fc6206e7efb13237bae5b766a03d69727f4
Alper Çugun <git@alper.nl>
2012-06-24 03:35:27 +0300

    changed a bunch of connections

---
+++ Gesjeesde Native/IssueSplashViewController.h
@@ -0,0 +1,18 @@
+//
+//  IssueSplashViewController.h
+//  Beestenbende
+//
+//  Created by Alper Çuğun on 24/6/12.
+//  Copyright (c) 2012 Hubbub. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SimpleAudioEngine.h"
+
+@interface IssueSplashViewController : UIViewController
+
+- (IBAction)done:(id)sender;
+- (IBAction)back:(id)sender;
+
+@end
---
+++ Gesjeesde Native/IssueSplashViewController.m
@@ -0,0 +1,55 @@
+//
+//  IssueSplashViewController.m
+//  Beestenbende
+//
+//  Created by Alper Çuğun on 24/6/12.
+//  Copyright (c) 2012 Hubbub. All rights reserved.
+//
+
+#import "IssueSplashViewController.h"
+
+@interface IssueSplashViewController ()
+
+@end
+
+@implementation IssueSplashViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+- (IBAction)done:(id)sender {
+    [[SimpleAudioEngine sharedEngine] playEffect:@"i02_schermverder.wav"];
+    
+    [self performSegueWithIdentifier:@"Next" sender:self];
+}
+
+- (IBAction)back:(id)sender {
+    [[SimpleAudioEngine sharedEngine] playEffect:@"i03_schermterug.wav"];
+    
+    [self.navigationController popViewControllerAnimated:YES];
+}
+
+@end
