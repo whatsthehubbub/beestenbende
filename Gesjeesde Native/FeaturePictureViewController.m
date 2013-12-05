bfc94d9deeb307cec08de1e9e56b070d5ff278e8
Alper Çugun <github@alper.nl>
2013-12-05 11:47:37 +0100

    reduce delay

--- Gesjeesde Native/FeaturePictureViewController.m
+++ Gesjeesde Native/FeaturePictureViewController.m
@@ -140,7 +140,7 @@
     self.takePictureAgainButton.hidden = NO;
     self.doneButton.hidden = NO;
     
-    double delayInSeconds = 0.5;
+    double delayInSeconds = 0.3;
     dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, (int64_t)(delayInSeconds * NSEC_PER_SEC));
     dispatch_after(popTime, dispatch_get_main_queue(), ^(void){
         self.doneButton.enabled = YES;
